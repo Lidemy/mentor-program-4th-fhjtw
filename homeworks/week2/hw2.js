a24d3c916184b9cfbcfbbe914f26e689edc9fdbf
fhjtw <fhjshirley@gmail.com>
2020-07-10 13:59:57 +0800

    第二週作業繳交，並修改hw2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,14 +1,5 @@
 function capitalize(str) {
-    var cap = ''
-    var first = str.charCodeAt(0)
-    if (first >= 97 && first <=122){
-        cap += String.fromCharCode(first- 32)
-        for(var i = 1; i < str.length; i++){
-            cap += str[i]
-        }
-        return cap;
-    }
-    return str;
+    return str[0].toUpperCase() + str.slice(1);
 }
 
 console.log(capitalize('hello'));
