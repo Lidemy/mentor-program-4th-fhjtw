43226c0ffee82769aee1b3cf42fcff47837050f5
fhjtw <54227769+fhjtw@users.noreply.github.com>
2020-07-12 20:33:45 +0800

    修改註解

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,13 +1,13 @@
 function join(arr, concatStr) {
-    if(arr.length === 0){ // 處理空字串
+    if(arr.length === 0){ // 處理空陣列
         return '';
     }
 
-    var joinArr = arr[0] //取首字元
-    for (var i = 1; i < arr.length; i++){ //只有一個字元時，僅印出首字元
+    var joinArr = arr[0] //取陣列第一個值
+    for (var i = 1; i < arr.length; i++){
         joinArr += concatStr + arr[i];
     }
-    return joinArr;
+    return joinArr; //如果陣列長度為1，不會進入上面for迴圈，直接回傳陣列第一個值
 
 }
 
